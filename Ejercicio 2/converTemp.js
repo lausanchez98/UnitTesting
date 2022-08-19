88d34811f7e89d6212b5f1306c461476e6924542
Laura Sánchez <lnsz.software@gmail.com>
2022-08-18 23:46:43 -0500

    Se modifica archivo converTemp.js para dar contexto del programa

--- Ejercicio 2/converTemp.js
+++ Ejercicio 2/converTemp.js
@@ -1,17 +1,18 @@
- 
- const celToKel = (ctemp) => {
-    let ktemp = ctemp + 273.15;
-    return ktemp;
- }
+// Conversor de temperatura Celsius a Kelvin
+const celToKel = (ctemp) => {
+  let ktemp = ctemp + 273.15;
+  return ktemp;
+};
 
- const kelToCel = (ktemp) => {
-    let ctemp = ktemp - 273;
-    return ctemp;
- }
+// Conversor de temperatura Kelvin a Celsius
+const kelToCel = (ktemp) => {
+  let ctemp = ktemp - 273;
+  return ctemp;
+};
 
- const convers = {
-    celToKel,
-    kelToCel
- }
+const convers = {
+  celToKel,
+  kelToCel,
+};
 
- module.exports = convers;
+module.exports = convers;
